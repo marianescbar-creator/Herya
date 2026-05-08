7c673ed719d79f64cc62570a02032b2f9ac40101
Marian Escobar <marianescobar.2003@gmail.com>
2026-05-08 22:30:10 +0200

    logo herya real en app y favicon

--- src/assets/HeryaLogo.jsx
+++ src/assets/HeryaLogo.jsx
@@ -1,22 +1,11 @@
+import heryaLogo from './herya-logo.png'
+
 export default function HeryaLogo({ className = '' }) {
   return (
-    <svg
-      viewBox="0 0 320 90"
-      xmlns="http://www.w3.org/2000/svg"
+    <img
+      src={heryaLogo}
+      alt="herya"
       className={className}
-      aria-label="herya"
-    >
-      <text
-        x="50%"
-        y="72"
-        textAnchor="middle"
-        fontFamily="'Bodoni Moda', 'Playfair Display', serif"
-        fontWeight="400"
-        fontSize="76"
-        fill="#b8389a"
-      >
-        herya
-      </text>
-    </svg>
+    />
   )
 }
